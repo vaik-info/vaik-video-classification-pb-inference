bd539a67226c7d8c1a5589d26971202d51b426f0
k-wakisaka <k.wakisaka@vaikobo.com>
2023-07-05 10:32:56 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # vaik-video-classification-pb-inference
 Inference by video classification PB model
+
+## Install
+
+```shell
+pip install git+https://github.com/vaik-info/vaik-video-classification-pb-inference.git
+```
+
+## Usage
+### Example
+
+```python
+import os
+import imageio
+from vaik_video_classification_pb_inference.pb_model import PbModel
+
+input_saved_model_dir_path = os.path.expanduser('~/.vaik-video-classification-pb-trainer/output_model/2023-07-05-09-27-55/step-1000_batch-8_epoch-11_loss_0.1745_sparse_categorical_accuracy_0.9555_val_loss_2.8367_val_sparse_categorical_accuracy_0.4620')
+classes = ("ApplyEyeMakeup", "ApplyLipstick", "Archery", "BabyCrawling", "BalanceBeam", "BandMarching", "BaseballPitch",
+           "Basketball", "BasketballDunk", "BenchPress", "Biking", "Billiards", "BlowDryHair", "BlowingCandles",
+           "BodyWeightSquats", "Bowling", "BoxingPunchingBag", "BoxingSpeedBag", "BreastStroke", "BrushingTeeth",
+           "CleanAndJerk", "CliffDiving", "CricketBowling", "CricketShot", "CuttingInKitchen")
+video_path = os.path.expanduser('/home/kentaro/.vaik-utc101-video-classification-dataset/test/Archery/Archery_1012.avi')
+
+model = PbModel(input_saved_model_dir_path, classes)
+
+video = imageio.get_reader(video_path,  'ffmpeg')
+frames = [frame for frame in video]
+output, raw_pred = model.inference(frames, batch_size=1)
+```
+
+### Output
+
+- output
+
+```
+[
+  {
+    'score': [
+      6.333147048950195,
+      5.063453674316406,
+      3.904092311859131,
+      ・・・
+      ],
+    'label': [
+      'CliffDiving',
+      'Basketball',
+      'Biking',
+      ・・・
+    ],
+    'start_frame': 0,
+    'end_frame': 16
+  },
+  ・・・
+```
+
+- raw_pred
+
+```
+[[-2.7884161e-01 -3.4559977e+00  2.3612885e+00  1.8890795e+00
+   8.8122386e-01  3.9267153e-01 -3.6665955e+00  5.0631857e+00
+  -4.7183666e+00 -1.6739460e+00  3.9042349e+00 -4.5220504e+00
+   2.6965277e+00 -6.0049925e+00  2.1489031e+00 -3.6779819e+00
+   ・・・
+```
---
+++ vaik_video_classification_pb_inference/pb_model.py
@@ -0,0 +1,78 @@
+from typing import List, Dict, Tuple
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+
+class PbModel:
+    def __init__(self, input_saved_model_dir_path: str = None, classes: Tuple = None):
+        self.model = tf.saved_model.load(input_saved_model_dir_path)
+        self.model_input_shape = self.model.signatures["serving_default"].inputs[0].shape
+        self.model_input_dtype = self.model.signatures["serving_default"].inputs[0].dtype
+        self.model_output_shape = self.model.signatures["serving_default"].outputs[0].shape
+        self.model_output_dtype = self.model.signatures["serving_default"].outputs[0].dtype
+        self.classes = classes
+
+    def inference(self, input_image_list: List[np.ndarray], batch_size: int = 8) -> Tuple[List[Dict], Dict]:
+        resized_image_array = self.__preprocess_image_list(input_image_list, self.model_input_shape[1:])
+        raw_pred = self.__inference(resized_image_array, batch_size)
+        output = self.__output_parse(raw_pred, len(input_image_list)-1)
+        return output, raw_pred
+
+    def __inference(self, resize_input_tensor: np.ndarray, batch_size: int) -> np.ndarray:
+        if len(resize_input_tensor.shape) != 5:
+            raise ValueError('dimension mismatch')
+        if not np.issubdtype(resize_input_tensor.dtype, np.uint8):
+            raise ValueError(f'dtype mismatch expected: {np.uint8}, actual: {resize_input_tensor.dtype}')
+
+        output_tensor = tf.zeros((resize_input_tensor.shape[0], self.model_output_shape[-1]),
+                                 self.model_output_dtype).numpy()
+        for index in range(0, resize_input_tensor.shape[0], batch_size):
+            batch = resize_input_tensor[index:index + batch_size, :, :, :]
+            batch_pad = tf.zeros(((batch_size, ) + self.model_input_shape[1:]), self.model_input_dtype).numpy()
+            batch_pad[:batch.shape[0], :, :, :] = batch
+            raw_pred = self.model(batch_pad)
+            output_tensor[index:index + batch.shape[0], :] = raw_pred[:batch.shape[0]]
+        return output_tensor
+
+    def __preprocess_image_list(self, input_image_list: List[np.ndarray],
+                                resize_input_shape: Tuple[int, int, int, int]) -> np.ndarray:
+        resized_image_list = []
+        for input_image in input_image_list:
+            resized_image = self.__preprocess_image(input_image, resize_input_shape[1:3])
+            resized_image_list.append(resized_image)
+        for _ in range(resize_input_shape[0] - (len(input_image_list) % resize_input_shape[0])):
+            resized_image_list.append(np.zeros(resize_input_shape[1:], dtype=np.uint8))
+        resized_image_array = np.split(np.stack(resized_image_list), len(resized_image_list)//resize_input_shape[0])
+        return np.stack(resized_image_array)
+
+    def __preprocess_image(self, input_image: np.ndarray, resize_input_shape: Tuple[int, int]) -> Tuple[
+        np.ndarray, Tuple[float, float]]:
+        if len(input_image.shape) != 3:
+            raise ValueError('dimension mismatch')
+        if not np.issubdtype(input_image.dtype, np.uint8):
+            raise ValueError(f'dtype mismatch expected: {np.uint8}, actual: {input_image.dtype}')
+
+        output_image = np.zeros((*resize_input_shape, input_image.shape[2]),
+                                dtype=input_image.dtype)
+        pil_image = Image.fromarray(input_image)
+        x_ratio, y_ratio = resize_input_shape[1] / pil_image.width, resize_input_shape[0] / pil_image.height
+        if x_ratio < y_ratio:
+            resize_size = (resize_input_shape[1], round(pil_image.height * x_ratio))
+        else:
+            resize_size = (round(pil_image.width * y_ratio), resize_input_shape[0])
+        resize_pil_image = pil_image.resize(resize_size)
+        resize_image = np.array(resize_pil_image)
+        output_image[:resize_image.shape[0], :resize_image.shape[1], :] = resize_image
+        return output_image
+
+    def __output_parse(self, pred: np.ndarray, max_frame_index: int) -> List[Dict]:
+        output_dict_list = []
+        pred_index = np.argsort(-pred, axis=-1)
+        for index in range(pred.shape[0]):
+            output_dict = {'score': pred[index][pred_index[index]].tolist(),
+                           'label': [self.classes[class_index] for class_index in pred_index[index]],
+                           'start_frame': index*self.model_input_shape[1],
+                            'end_frame': min(index*self.model_input_shape[1]+self.model_input_shape[1], max_frame_index)}
+            output_dict_list.append(output_dict)
+        return output_dict_list
